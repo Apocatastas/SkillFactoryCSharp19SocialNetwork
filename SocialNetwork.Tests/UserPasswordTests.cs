23326f8047ada804d914f856f591972902c33bef
ɐllɐ <molchanova.a.a@gmail.com>
2025-01-01 22:45:29 +0300

    19.5.1 - HOMEWORK (PJ-03) - final tests added

---
+++ SocialNetwork.Tests/UserPasswordTests.cs
@@ -0,0 +1,22 @@
+﻿using SocialNetwork.DAL.Entities;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class UserPasswordTests
+    {
+        [Test]
+        public void PasswordLengthIsMoreThan8Symbols()
+        {
+            var user = new UserEntity()
+            {
+                firstname = "Yegor",
+                lastname = "Letov",
+                email = "ooo@moya-oboro.na",
+                password = "vintovka"
+            };
+
+            Assert.That(user.password.Length >= 8);
+        }
+    }
+}
